9bcf05920975587bffebb957f000ec1607153b09
Satendra Singh <satendras0912@gmail.com>
2026-02-28 15:57:52 +0530

    add sitemap and robots configuration

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://salessetu.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://salessetu.in/real-estate-sales-acceleration",
+      lastModified: new Date(),
+    },
+  ];
+}
